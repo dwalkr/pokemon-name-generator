9a1e0e1fa07f9de10cbb2c5cc57fb3b67d70ddac
dj <dj@gullinbursti>
2016-07-26 23:48:02 -0400

    responsive meta tag

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
         <title>Pokemon Name Generator</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <link rel="stylesheet" href="css/style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     <head>
     <body>
         <div class="container">
